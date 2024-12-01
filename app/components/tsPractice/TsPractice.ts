7d8c818eddd19309d5a56744abcc177c2cce8b4c
Umar Farooq <uf29664@gmail.com>
2024-12-01 22:09:10 +0500

    portfolio

---
+++ app/components/tsPractice/TsPractice.ts
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function TsPractice() {
+  return (
+    <div>
+      <h1 className="">TypeScript</h1>
+    </div>
+  )
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,21 @@
-'use client'
-import {  useState } from "react";
+// 'use client'
+// import {  useState } from "react";
+
 
 export default function Home() {
-  const [store,setStore]=useState<number | null>(null)
-  const [first,setFirst]=useState('')
-  const [second,setSecond]=useState('')
-  const a = parseFloat(first)
-  const b = parseFloat(second)
-  let str = 'umar';
-  str='ali'
-  str='rizwan'
-  console.log(str);
 
-  
-const addHandler=(a:number,b:number)=>{
-  const result = a + b
-  setStore(result)
-}
+
+
+
+
   
   
   return (
     <div className="">
-      <input type="text" placeholder="Enter Your First Value" onChange={(e)=>setFirst(e.target.value)} />
+      {/* <input type="text" placeholder="Enter Your First Value" onChange={(e)=>setFirst(e.target.value)} />
       <input type="text" placeholder="Enter Your Second Value" onChange={(e)=>setSecond(e.target.value)} />
-      <button onClick={()=>addHandler(a,b)}>Add</button>
-      <p>Add : {store}</p>
+      <button onClick={()=>addHandler(a,b)}>Add</button> */}
+      {/* <p>Add : {store}</p> */}
 
     </div>
   );
