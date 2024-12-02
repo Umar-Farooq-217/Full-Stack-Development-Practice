fae3996ed8d7502c415509da27bb5e4548f24cb5
Umar Farooq <uf29664@gmail.com>
2024-12-02 22:23:08 +0500

    portfolio

--- app/components/tsPractice/TsPractice.tsx
+++ app/components/tsPractice/TsPractice.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function TsPractice() {
   return (
     <div>
-      <h1 className="">TypeScript</h1>
+    <h1 className="">Umar Farooq</h1>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-// 'use client'
-// import {  useState } from "react";
+
+
+import TsPractice from "./components/tsPractice/TsPractice";
 
 
 export default function Home() {
@@ -11,11 +12,9 @@ export default function Home() {
   
   
   return (
-    <div className="">
-      {/* <input type="text" placeholder="Enter Your First Value" onChange={(e)=>setFirst(e.target.value)} />
-      <input type="text" placeholder="Enter Your Second Value" onChange={(e)=>setSecond(e.target.value)} />
-      <button onClick={()=>addHandler(a,b)}>Add</button> */}
-      {/* <p>Add : {store}</p> */}
+    <div className="bg-blue-300">
+      <TsPractice/>
+   
 
     </div>
   );
