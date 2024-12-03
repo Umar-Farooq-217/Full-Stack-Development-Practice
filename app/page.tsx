5c9e37fe1e7978d0afebc00ee3620447f125b20c
Umar Farooq <uf29664@gmail.com>
2024-12-03 21:15:49 +0500

    practice

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-
+import React from "react";
 
 import TsPractice from "./components/tsPractice/TsPractice";
 
 
 export default function Home() {
+  //Optional params by (?)
+// const greet =(name:string,id?:number)=>{
+
+// Default Params => params:type = value
+// const greet =(name:string,id:number = 1)=>{
+const greet =(name:string,id?:number)=>{
+  return {name,id}
+  
+}
+console.log(greet('umar'));
 
 
 
@@ -12,7 +22,7 @@ export default function Home() {
   
   
   return (
-    <div className="bg-blue-300">
+    <div >
       <TsPractice/>
    
 
