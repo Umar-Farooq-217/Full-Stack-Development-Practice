931bf95bae7c38be05ab7842b38e331321bbc2fd
Umar Farooq <uf29664@gmail.com>
2024-12-16 20:52:22 +0500

    practice

--- app/components/jsPractice/JsPractice.js
+++ app/components/jsPractice/JsPractice.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function JsPractice() {
     
-
-
 function twoSums (nums,target){
     for (let index = 0; index < nums.length; index++) {
         const compliment =target- nums[index] 
@@ -12,14 +10,16 @@ function twoSums (nums,target){
        for (let j = 0; j < nums.length; j++) {
         const element = nums[j] ;
         console.log(`index ${j} , Number : ${nums[j]} and substraction : ${element}`); 
+        if (nums[j] === compliment) {
+          return [index, j]; 
+      }
     }
-   
+    
 }
 var nums = [3, 2, 4];
 var target = 6;
-const result = twoSums(nums, target);
 
-console.log(result);
+console.log(twoSums(nums, target));
 
   return (
     <div>
