84a122db70b4d8383b0b287ef8b82266990bf215
Umar Farooq <uf29664@gmail.com>
2024-12-16 21:25:01 +0500

    practice

--- app/components/jsPractice/JsPractice.js
+++ app/components/jsPractice/JsPractice.js
@@ -1,25 +1,13 @@
 import React from 'react'
 
 export default function JsPractice() {
-    
-function twoSums (nums,target){
-    for (let index = 0; index < nums.length; index++) {
-        const compliment =target- nums[index] 
-       console.log(`index ${index} , Number : ${nums[index]} and substraction : ${compliment}`);
-      
-       for (let j = 0; j < nums.length; j++) {
-        const element = nums[j] ;
-        console.log(`index ${j} , Number : ${nums[j]} and substraction : ${element}`); 
-        if (nums[j] === compliment) {
-          return [index, j]; 
-      }
-    }
-    
-}
-var nums = [3, 2, 4];
-var target = 6;
-
-console.log(twoSums(nums, target));
+ const names = ["umar","ali" ,"rizwan" , "faroow"]
+ for (let index = 0; index < names.length; index++) {
+  const element = names[index];
+  console.log(element,'is a good boy');
+  
+  
+ }
 
   return (
     <div>
@@ -28,4 +16,3 @@ console.log(twoSums(nums, target));
   )
 }
 
-}
