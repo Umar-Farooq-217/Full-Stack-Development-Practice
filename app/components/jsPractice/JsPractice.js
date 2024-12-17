44e8f240fb10cc8b0fdc4befe152d02c160026d4
Umar Farooq <uf29664@gmail.com>
2024-12-17 20:42:22 +0500

    practice

--- app/components/jsPractice/JsPractice.js
+++ app/components/jsPractice/JsPractice.js
@@ -1,13 +1,7 @@
 import React from 'react'
 
 export default function JsPractice() {
- const names = ["umar","ali" ,"rizwan" , "faroow"]
- for (let index = 0; index < names.length; index++) {
-  const element = names[index];
-  console.log(element,'is a good boy');
-  
-  
- }
+
 
   return (
     <div>
