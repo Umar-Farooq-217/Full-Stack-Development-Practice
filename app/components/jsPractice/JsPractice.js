4b90c4505cd81ccef56f8ee8218c1b2c328f11a1
Umar Farooq <uf29664@gmail.com>
2024-12-17 21:26:16 +0500

    practice

--- app/components/jsPractice/JsPractice.js
+++ app/components/jsPractice/JsPractice.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 export default function JsPractice() {
 
+const names = (message)=>{
+  // console.log(' function delayed');
+  console.log(message);}
+console.log('before');
 
+
+setTimeout(() => names('Hello, this is a delayed function!'), 300);
+// clearTimeout(setTimeout)
+console.log('after');
   return (
     <div>
       
