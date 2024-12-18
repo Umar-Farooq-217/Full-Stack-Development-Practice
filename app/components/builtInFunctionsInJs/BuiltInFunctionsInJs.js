bd7ec531dcbe9b5b12fcf96c74b0d1805c9878e3
Umar Farooq <uf29664@gmail.com>
2024-12-18 11:55:01 +0500

    practice

---
+++ app/components/builtInFunctionsInJs/BuiltInFunctionsInJs.js
@@ -0,0 +1,37 @@
+'use client'
+import React from 'react'
+
+export default function BuiltInFunctionsInJs() {
+    // Math.random()
+    // var r = Math.random();
+    // var a1 = 1 ;
+    // var b1 = 100;
+    // var r1 = a1 + (b1 - a1)*Math.random()
+    // console.log(r1);
+
+    // min and max function
+    // var arr = [3, 6,3, 6,8, 9]
+    // console.log(Math.min(3, 6,3, 6,8, 9));
+    // console.log(Math.max(3, 6,3, 6,8, 9));
+
+    // unshift() and shift() add data on first index and remove data from first index
+    // var arra = [0 ,2,3,4,5]
+    // arra.unshift(9) // add at begining of array
+    // console.log(arra);
+    // arra.shift() // remove from begining of array
+    // console.log(arra);
+
+    // push() and pop() add and remove data at last of array 
+    // arra.push(34)
+    // console.log(arra);
+    // arra.pop()
+    // console.log(arra);
+
+    
+
+  return (
+    <div>
+      Built In Functions in Js
+    </div>
+  )
+}
---
+++ app/page.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+// import TsPractice from "./components/tsPractice/TsPractice";
+// import JsPractice from './components/jsPractice/JsPractice'
+import BuiltInFunctionsInJs from './components/builtInFunctionsInJs/BuiltInFunctionsInJs'
+
+export default function Home() {
+
+
+
+  
+  return (
+    <div >
+      {/* <TsPractice/>
+      <JsPractice/> */}
+      <BuiltInFunctionsInJs/>
+   
+
+    </div>
+  );
+}
