d3354a6d6bef833f470a16e9b51316978c429f45
Umar Farooq <uf29664@gmail.com>
2025-01-02 22:17:56 +0500

    flipCard

---
+++ app/components/cards/flipCard/FlipCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function FlipCard() {
+  return (
+    <div>
+      <h1>Flip Card</h1>
+      <div className="card">
+        <div className="card-front-side">
+            <h1>card front side</h1>
+        </div>
+        <div className="card-back-side">
+            <h1>Card Back Side</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
